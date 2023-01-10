8c7a66d213cf662fe8380086d6eb4d7951a48cab
Monica Taylor <czechowm@gmail.com>
2023-01-09 20:50:11 -0500

    password visibility toggle added

--- public/js/main.js
+++ public/js/main.js
@@ -67,3 +67,17 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+
+//Password Toggle Icon
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("fa-eye");
+});
